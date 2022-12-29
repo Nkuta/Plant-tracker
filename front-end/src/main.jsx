205ed716b5503c2d3522b031a423d2d9092b87a4
bonface221 <mainaboniface221@gmail.com>
2022-12-29 11:38:45 +0300

    ft:added the bootstrap js dependancy

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.css";
+// Bootstrap js
+import "bootstrap/dist/js/bootstrap.bundle.min";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
